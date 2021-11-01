4880aca1a0d54c0c93f0e2cf144bbfae7e538ee3
Raman Preet Singh <ramanpre0810@gmail.com>
2021-11-01 12:01:03 +0530

    Completed 1st phase

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+const httpserver = require('http').createServer(app);
+const io = require('socket.io')(httpserver, {
+	cors: {
+		origin: ['http://localhost:8080', 'https://admin.socket.io'],
+		methods: ['*'],
+	},
+});
+
+const { instrument } = require('@socket.io/admin-ui');
+
+io.on('connection', socket => {
+	console.log(socket.id);
+
+	socket.on('key-pressed', (request, cb) => {
+		console.log(request);
+		setTimeout(() => {
+			const result = { value: request * 2 };
+			cb(result);
+		}, 3000);
+	});
+});
+
+app.get('/status', (req, res, next) => {
+	res.json({
+		status: 'OK',
+		date: `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`,
+	});
+});
+
+httpserver.listen(3000);
+
+instrument(io, { auth: false });
